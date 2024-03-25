10dfbc3c85601efeeb5b318307d57b5275dc8224
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2024-03-25 18:43:43 -0400

    Get location from browser, query nearby observations with lat long

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import RegionSelect from './components/RegionSelect';
 import RegionalStats from './components/RegionalStats';
 import ChecklistFeed from './components/ChecklistFeed';
 import useFetch from './hooks/useFetch';
 import './App.css'
 import { EbirdRegion } from './types';
+import NearbyObservations from './components/NearbyObservations';
 
 function App() {
   const [selectedState, setSelectedState] = useState('');
   const [selectedRegion, setSelectedRegion] = useState('');
-  const { data: states, isLoading, error } = useFetch("https://api.ebird.org/v2/ref/region/list/subnational1/US")
+  const [hasLocation, setHasLocation] = useState(false);
+  const [latLong, setLatLong] = useState({ lat: '', long: '' });
+  const { data: states, isLoading, error } = useFetch("https://api.ebird.org/v2/ref/region/list/subnational1/US");
 
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      try {
+        navigator.geolocation.getCurrentPosition((position) => {
+          // if (position) {
+          setHasLocation(true);
+          const lat = position.coords.latitude.toFixed(2);
+          const long = position.coords.longitude.toFixed(2);
+          setLatLong({ lat, long });
+          // }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [])
   // Get states: https://api.ebird.org/v2/ref/region/list/subnational1/US
   // Get regions in states: https://api.ebird.org/v2/ref/region/list/subnational2/US-NY
   // Get adjacent regions: https://api.ebird.org/v2/ref/adjacent/US-NY
 
   return (
     <>
+      {hasLocation && <NearbyObservations lat={latLong.lat} long={latLong.long} />}
       <div className="card">
         <label htmlFor="ebirdStates">Choose a state</label>
         <select id="ebirdStates" onChange={(e) => {
@@ -30,9 +50,6 @@ function App() {
         {selectedRegion && <RegionalStats selectedRegion={selectedRegion} />}
         {selectedRegion && <ChecklistFeed selectedRegion={selectedRegion} />}
       </div>
-
-      {/* 
-      <div>{checklist?.obs?.map(({ howManyStr, speciesCode }) => <p key={speciesCode}>{speciesCode}: {howManyStr}</p>)}</div> */}
     </>
   )
 }
---
+++ src/components/NearbyObservations.tsx
@@ -0,0 +1,43 @@
+import useFetch from "../hooks/useFetch";
+
+type Location = { lat: string; long: string; }
+
+type Observation = {
+    subId: string;
+    comName: string;
+    sciName: string;
+    locName: string;
+    howMany: number;
+    obsDt: string;
+    speciesCode: string;
+}
+
+const NearbyObservations = ({ lat, long }: Location) => {
+    const { data: nearby, isLoading } = useFetch(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${lat}&lng=${long}&maxResults=50&dist=5`);
+    console.log(nearby);
+    return (
+        <div>
+            {isLoading && <div>nearby observations requested</div>}
+            {nearby?.map((obs: Observation) => {
+                const date = new Date(obs.obsDt);
+                const options: Intl.DateTimeFormatOptions = {
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                };
+                const formatted = date.toLocaleDateString(undefined, options);
+                return (
+                    <div className="observation" key={obs.subId + obs.speciesCode}>
+                        <p>{obs.howMany}</p>
+                        <div>
+                            <p>{obs.comName} ({obs.sciName})</p>
+                            <p>{obs.locName}</p>
+                            <p>on {formatted}</p>
+                        </div>
+                    </div>)
+            })}
+        </div>
+    )
+}
+
+export default NearbyObservations;
