9f0dcf925935a210c1d348d9504dc27e7ba7ac6b
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2024-06-29 14:29:15 -0400

    Make date nav keyboard-navigable

--- src/components/ObservationsByDate.tsx
+++ src/components/ObservationsByDate.tsx
@@ -4,6 +4,7 @@ import { formatDateNav } from "../helpers";
 import DateDetail from "./DateDetail";
 
 const ObservationsByDate = () => {
+    const dayElements = [...document.querySelectorAll<HTMLElement>(".date-nav")];
     const {
         obs: birds,
         birdMap
@@ -31,12 +32,36 @@ const ObservationsByDate = () => {
                         const numLocations = [...dayData.keys()].length;
                         let totalSpecies = 0;
                         for (const entry of dayData) {
+                            // add up the number of species seen on each day
                             totalSpecies += entry[1].length;
                         }
                         return (
-                            <div key={day} tabIndex={0} className={`date-nav ${day === currentDay ? 'date-selected' : ''}`} onClick={() => setCurrentDay(day)}>
+                            <div
+                                data-day={day}
+                                key={day}
+                                tabIndex={0}
+                                className={`date-nav ${day === currentDay ? 'date-selected' : ''}`}
+                                onKeyDown={(e) => {
+                                    if (document.activeElement?.getAttribute("class")?.includes("date-nav")) {
+                                        if (e.code === "Enter") { setCurrentDay(day) };
+                                        if (e.code === "ArrowUp" || e.code === "ArrowDown") {
+                                            const index = dayElements.findIndex((item) => item.dataset.day === day);
+                                            if (index >= 0 && e.code === "ArrowUp") {
+                                                const newIndex = (index - 1 + dayElements.length) % dayElements.length;
+                                                dayElements[newIndex].focus();
+                                            };
+                                            if (index >= 0 && e.code === "ArrowDown") {
+                                                const newIndex = (index + 1) % dayElements.length;
+                                                dayElements[newIndex].focus();
+                                            };
+                                        }
+                                    }
+                                }}
+                                onClick={() => setCurrentDay(day)}>
                                 <h3 className="date-nav-heading">{formatDateNav(day)}</h3>
-                                <p className="date-nav-stats">{numLocations} location{numLocations > 1 ? 's' : ''} • {totalSpecies} species</p>
+                                <p className="date-nav-stats">
+                                    {numLocations} location{numLocations > 1 ? 's' : ''} • {totalSpecies} species
+                                </p>
                             </div>
                         )
                     })}
